8003a1dc004d9e4575cceb5139ff727a99813f0d
Rahmi Fauziah Rambe <fauziahrahmi933@gmail.com>
2024-05-22 13:20:15 +0700

    praktikum9

---
+++ resources/views/admin/partials/_footer.blade.php
@@ -0,0 +1,5 @@
+  <!-- ======= Footer ======= -->
+  <footer id="footer" class="footer">
+    <div class="copyright">
+      &copy; Copyright <strong><span>Rahmi Fauziah Rambe</span></strong>. All Rights Reserved
+  </footer><!-- End Footer -->
